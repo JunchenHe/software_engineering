6367ed93f3493a98e17eccaded7fa70542c3ffa0
JunchenHe <850463594@qq.com>
2018-05-20 18:44:58 +0800

    文档跟新

--- server/background_manager/background_manager/Form1.Designer.cs
+++ server/background_manager/background_manager/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(245, 44);
             this.button1.TabIndex = 0;
-            this.button1.Text = "连接数据库插入数据";
+            this.button1.Text = "发布消息";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
